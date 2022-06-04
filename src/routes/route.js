b4a88b8d02543caa157b294eaf99bb36124610a0
dahifaleshubham <dahifaleshubham@gmail.com>
2022-06-05 00:04:14 +1200

    module system

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,36 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+ // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+ router.get("/sol1", function (req, res) {
 
- 
- let  players = []
-
-router.post('/players', function (req, res) {
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+  
+  
+  
+  // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+  router.get("/sol2", function (req, res) {
     
-    let newPlayer = req.body
-    let newPlayersName = newPlayer.name
-    let isNameRepeated = false
-
-    //let player = players.find(p => p.name == newPlayersName)
-
-    for(let i = 0; i < players.length; i++) {
-        if(players[i].name == newPlayersName) {
-            isNameRepeated = true;
-            break;
-        }
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
     }
-
-    //undefined is same as false/ a falsy value
-    if (isNameRepeated) {
-        //Player exists
-        res.send("This player was already added!")
-    } else {
-        //New entry
-        players.push(newPlayer)
-        res.send(players)
-    }
-});
- 
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+  
  
 
 module.exports = router;
